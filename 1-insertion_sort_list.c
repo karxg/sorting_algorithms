f684844d3c5fb7ed53dd2613dc39e8722789d2d5
karxg <karim4420@aol.com>
2024-02-20 18:05:18 +0200

    1Fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,10 +12,10 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *temp;
 
-	current = (*list)->next;
-
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	return;
+
+	current = (*list)->next;
 
 	while (current)
 	{
